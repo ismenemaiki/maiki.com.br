b8b4ba01d4e448e4974be1245e33822e08d59891
Maiki Ismene <ismenemaiki@gmail.com>
2019-11-23 12:53:24 -0200

    css home

--- maiki/src/app/app.module.ts
+++ maiki/src/app/app.module.ts
@@ -4,12 +4,14 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { MenuComponent } from './menu/menu.component';
 import { HomeComponent } from './home/home.component';
+import { SobreComponent } from './sobre/sobre.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     MenuComponent,
-    HomeComponent
+    HomeComponent,
+    SobreComponent
   ],
   imports: [
     BrowserModule
