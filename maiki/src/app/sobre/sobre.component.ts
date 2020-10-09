d2c60a67e4314a0d095b01c7633d07e852fdd46f
Maiki <ismenemaiki@gmail.com>
2020-10-09 18:33:20 -0300

    criação de conteudo do sobre

--- maiki/src/app/sobre/sobre.component.ts
+++ maiki/src/app/sobre/sobre.component.ts
@@ -6,7 +6,17 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./sobre.component.css']
 })
 export class SobreComponent implements OnInit {
-
+  rootImagens = '../../assets/img/redes-sociais/';
+  arrayRedes = [
+    { nome: 'facebook', link: 'https://www.facebook.com/maiki.rdgs' },
+    { nome: 'instagram', link: 'https://www.instagram.com/maiki_rdgs/' },
+    { nome: 'whatsapp', link: 'https://wa.me/5511948082374' },
+    { nome: 'linkedin', link: 'https://www.linkedin.com/in/maiki-ismene-495737110/' },
+    { nome: 'pinterest', link: '' },
+    { nome: 'skype', link: '' },
+    { nome: 'email', link: 'mailto:ismenemaiki@gmail.com' },
+    { nome: 'github', link: 'https://github.com/ismenemaiki' }
+  ];
   constructor() { }
 
   ngOnInit() {
