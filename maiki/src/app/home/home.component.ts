5b4d78e693d319232170cd2c455b2e41714afdd1
MAIKI <ismenemaiki@gmail.com>
2021-02-08 16:34:11 -0300

    inserido novo conteudo com novo layout

--- maiki/src/app/home/home.component.ts
+++ maiki/src/app/home/home.component.ts
@@ -3,13 +3,22 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.css']
+  styleUrls: ['./home.component.css'],
 })
 export class HomeComponent implements OnInit {
-
-  constructor() { }
+  constructor() {}
 
   ngOnInit() {
+    const titulo = document.querySelector('.console-pc');
+    this.typeWrite(titulo);
   }
-
+  typeWrite(elemento: any) {
+    const textoArray = elemento.innerHTML.split('');
+    elemento.innerHTML = ' ';
+    textoArray.forEach((letra: string, i: number) => {
+      setTimeout(() => {
+        elemento.innerHTML += letra;
+      }, 75 * i);
+    });
+  }
 }
