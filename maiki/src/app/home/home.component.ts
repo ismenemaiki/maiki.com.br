24c5e8f0573974058a78ecd6fa174d910d9f3b1e
MAIKI <ismenemaiki@gmail.com>
2021-02-09 17:09:42 -0300

    remoção dos medias queries

--- maiki/src/app/home/home.component.ts
+++ maiki/src/app/home/home.component.ts
@@ -1,20 +1,25 @@
 import { BrowserDetectService } from './../core/services/browser-detect.service';
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, AfterViewInit } from '@angular/core';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css'],
 })
-export class HomeComponent implements OnInit {
-  isMobile: boolean;
+export class HomeComponent implements OnInit, AfterViewInit {
+  isMobile: boolean = false;
+  isDesktop: boolean = false;
 
   constructor(private browserDetect: BrowserDetectService) {}
 
   ngOnInit() {
     this.isMobile = this.browserDetect.isMobile();
-    const titulo = document.querySelector('.console-pc');
-    this.typeWrite(titulo);
+    this.isDesktop = this.browserDetect.isDesktop();
+  }
+
+  ngAfterViewInit() {
+    const textoConsole = document.querySelector('.console-pc');
+    this.typeWrite(textoConsole);
   }
   typeWrite(elemento: any) {
     const textoArray = elemento.innerHTML.split('');
