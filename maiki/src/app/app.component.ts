d2384be0da59cba53265654e0c0971c2affe7e1d
MAIKI <ismenemaiki@gmail.com>
2021-02-10 14:48:00 -0300

    adicionado menu mobile

--- maiki/src/app/app.component.ts
+++ maiki/src/app/app.component.ts
@@ -1,10 +1,21 @@
-import { Component } from '@angular/core';
+import { BrowserDetectService } from './core/services/browser-detect.service';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'maiki';
+
+  isMobile: boolean = false;
+  isDesktop: boolean = false;
+
+  constructor(public browserDetect: BrowserDetectService) {}
+
+  ngOnInit() {
+    this.isMobile = this.browserDetect.isMobile();
+    this.isDesktop = this.browserDetect.isDesktop();
+  }
 }
--- maiki/src/app/menu/menu.component.ts
+++ maiki/src/app/menu/menu.component.ts
@@ -1,35 +1,36 @@
+import { BrowserDetectService } from './../core/services/browser-detect.service';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-menu',
   templateUrl: './menu.component.html',
-  styleUrls: ['./menu.component.css']
+  styleUrls: ['./menu.component.css'],
 })
 export class MenuComponent implements OnInit {
-  constructor() {}
-  listaMenu = ['inicio', 'sobre', 'portfolios', 'curriculo', 'blog'];
+  isMobile = false;
+  isDesktop = false;
+
+  constructor(private browserDetect: BrowserDetectService) {}
+
   ngOnInit() {
-    // const inicio = document.getElementById('inicio');
-    // inicio.className += ' active';
+    this.isMobile = this.browserDetect.isMobile();
+    this.isDesktop = this.browserDetect.isDesktop();
   }
 
-  menuResponsivo() {
-    const menuNav = document.getElementById('menu-nav');
-    if (menuNav.className === 'menu-nav') {
-      menuNav.className += ' responsive';
-    } else {
-      menuNav.className = 'menu-nav';
-    }
+  openMenu() {
+    document.getElementById('mySidepanel').style.width = '70%';
+    document.getElementById('mySidepanel').style.height = '100%';
+    setTimeout(() => {
+      document.addEventListener('click', this.handleClickOutside, true);
+    }, 100);
   }
-  ativar(menu: string) {
-    this.menuResponsivo();
-    this.listaMenu.forEach((it: string) => {
-      const item = document.querySelector(`#${it}`);
-      if (item.className.includes('active')) {
-        item.className = '';
-      }
-    });
-    const menuClicado = document.querySelector(`#${menu}`);
-    menuClicado.className += ' active';
+
+  closeMenu() {
+    document.getElementById('mySidepanel').style.width = '0';
+  }
+
+  handleClickOutside() {
+    document.getElementById('mySidepanel').style.width = '0';
+    document.removeEventListener('click', this.handleClickOutside, false);
   }
 }
