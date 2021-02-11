157efb1360525e114232197ae74fa1c4ad3e79b4
MAIKI <ismenemaiki@gmail.com>
2021-02-11 11:16:30 -0300

    adicionado rodape mobile

--- maiki/src/app/rodape/rodape.component.ts
+++ maiki/src/app/rodape/rodape.component.ts
@@ -1,3 +1,4 @@
+import { BrowserDetectService } from './../core/services/browser-detect.service';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
@@ -6,6 +7,9 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./rodape.component.css']
 })
 export class RodapeComponent implements OnInit {
+  isDesktop = false;
+  isMobile = false;
+
   rootImagens = '../../assets/img/redes-sociais/';
   arrayRedes = [
     { nome: 'facebook', link: 'https://www.facebook.com/maiki.rdgs' },
@@ -17,9 +21,17 @@ export class RodapeComponent implements OnInit {
     { nome: 'whatsapp', link: 'https://wa.me/5511948082374' },
     { nome: 'email', link: 'mailto:ismenemaiki@gmail.com' },
   ];
-  constructor() { }
+  arrayMobile = [
+    { nome: 'whatsapp', link: 'https://wa.me/5511948082374' },
+    { nome: 'email', link: 'mailto:ismenemaiki@gmail.com' },
+    { nome: 'facebook', link: 'https://www.facebook.com/maiki.rdgs' },
+    { nome: 'linkedin', link: 'https://www.linkedin.com/in/maiki-ismene-495737110/' },
+    { nome: 'instagram', link: 'https://www.instagram.com/maiki_rdgs/' },
+  ];
+  constructor(private browser: BrowserDetectService) { }
 
   ngOnInit() {
+    this.isDesktop = this.browser.isDesktop();
+    this.isMobile = this.browser.isMobile();
   }
-
 }
