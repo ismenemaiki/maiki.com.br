6f32825c8f368b3d06aa50fc2b2a98624ead45f5
MAIKI <ismenemaiki@gmail.com>
2021-02-22 20:38:27 -0300

    ajustes portfolios mobile e desk

--- maiki/src/app/portfolios/portfolios.component.ts
+++ maiki/src/app/portfolios/portfolios.component.ts
@@ -1,88 +1,77 @@
-import { AfterViewInit, Component, OnInit } from '@angular/core';
+import { BrowserDetectService } from './../core/services/browser-detect.service';
+import { Component, OnInit, OnChanges } from '@angular/core';
+import Swiper from 'swiper';
+import 'swiper/swiper-bundle.css';
+import SwiperCore, {
+  Navigation,
+  Pagination,
+  EffectFlip,
+  HashNavigation,
+  Autoplay
+} from 'swiper/core';
 
+// install Swiper modules
+SwiperCore.use([Navigation, Pagination, EffectFlip, HashNavigation, Autoplay ]);
 @Component({
   selector: 'app-portfolios',
   templateUrl: './portfolios.component.html',
   styleUrls: ['./portfolios.component.css'],
 })
-export class PortfoliosComponent implements OnInit, AfterViewInit {
-  tattoos: any;
+export class PortfoliosComponent implements OnInit, OnChanges {
+  isMobile = false;
+  isDesktop = false;
+
+  tattoos: boolean = true;
   desenhos: any;
-  slideIndex = 1;
-  arrayTattoos: any;
-  arrayDesenhos: any;
-  rootTattos = '../../assets/img/tattoos/';
-  // rootDesenhos = '../../assets/img/desenhos/';
-  imagemSelecionada: string;
 
-  constructor() {}
+  constructor(private browserDetect: BrowserDetectService) {}
 
+  ngOnChanges() {
+
+  }
   ngOnInit() {
-    this.arrayTattoos = [
-      { id: 1, nome: 'livrai-me-do-mal' },
-      { id: 2, nome: 'gengar' },
-      { id: 3, nome: 'triangulo-bolinha' },
-      { id: 4, nome: 'bem-me-quero' },
-      { id: 5, nome: 'chora-agora' },
-      { id: 6, nome: 'decidi' },
-      { id: 7, nome: 'filhos-amor-eterno' },
-      { id: 8, nome: 'hakuna-matata' },
-      { id: 9, nome: 'irmaos-metralha' },
-      { id: 10, nome: 'jamais-perca-seu-equilibrio' },
-      { id: 11, nome: 'la-casa-de-papel' },
-      { id: 12, nome: 'palhaço' },
-      { id: 13, nome: 'passarinho-rosa' },
-    ];
-    this.arrayDesenhos = [
-      { id: 1, nome: 'img' }
-    ];
-    const abaTattoo = document.getElementById('btn-tattoo');
-    abaTattoo.click();
-  }
-  ngAfterViewInit() {
-    this.exibirImagem(this.slideIndex);
+    this.isDesktop = this.browserDetect.isDesktop();
+    this.isMobile = this.browserDetect.isMobile();
+    window.onscroll = () => { this.browserDetect.scrollFunction(50); };
+
+    setTimeout(() => {
+      const swiper = new Swiper('.swiper-container', {
+        effect: 'flip',
+        grabCursor: true,
+        hashNavigation: { watchState: true },
+        pagination: {
+          el: '.swiper-pagination',
+          clickable: true,
+        },
+        // autoplay: {
+        //   delay: 4000,
+        //   disableOnInteraction: true,
+        // },
+        navigation: {
+          nextEl: '.swiper-button-next',
+          prevEl: '.swiper-button-prev',
+        },
+      });
+  }, 100);
 
   }
-  abrirModal(id: string, imagem: string) {
-    this.imagemSelecionada = imagem;
-    document.getElementById(id).style.display = 'block';
-  }
-  fecharModal(id: string) {
-    document.getElementById(id).style.display = 'none';
-  }
-  trocaImagem(n: number) {
-    this.exibirImagem((this.slideIndex += n));
-  }
-  exibirImagem(n: number) {
-    let i: number;
-    const slide: any = document.getElementsByClassName('slides');
-    const selecionado: any = document.getElementsByClassName('selecionado');
 
-    if (n > slide.length) { this.slideIndex = 1; }
-    if (n < 1) { this.slideIndex = slide.length; }
-    for (i = 0; i < slide.length; i++) {  slide[i].style.display = 'none'; }
-    for (i = 0; i < selecionado.length; i++) {
-      selecionado[i].className = selecionado[i].className.replace(' atual', '');
+  // onSwiper(swiper) {
+  //   console.log(swiper);
+  // }
+  // onSlideChange() {
+  //   console.log('slide change');
+  // }
+  changeAba(aba: string): void {
+    if (aba === 'desenhos') {
+      this.tattoos = false;
+      this.desenhos = true;
+      this.ngOnInit();
     }
-    slide[this.slideIndex - 1].style.display = 'block';
-    selecionado[this.slideIndex - 1].className += ' atual';
-  }
-  imagemAtual(n: number) {
-    this.exibirImagem((this.slideIndex = n));
-  }
-  trocaAba(evt: MouseEvent, aba: string) {
-    let i: number;
-    let conteudoAba: any;
-    let linkAba: any;
-    conteudoAba = document.getElementsByClassName('conteudoAba');
-    for (i = 0; i < conteudoAba.length; i++) {
-      conteudoAba[i].style.display = 'none';
+    if (aba === 'tattoos') {
+      this.tattoos = true;
+      this.desenhos = false;
+      this.ngOnInit();
     }
-    linkAba = document.getElementsByClassName('linkAba');
-    for (i = 0; i < linkAba.length; i++) {
-      linkAba[i].className = linkAba[i].className.replace(' active', '');
-    }
-    document.getElementById(aba).style.display = 'block';
-    (evt.currentTarget as any).className += ' active';
   }
 }
