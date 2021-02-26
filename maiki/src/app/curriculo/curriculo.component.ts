0a8122b0126940afa50f67215f4b0c55e05e79ea
MAIKI <ismenemaiki@gmail.com>
2021-02-26 19:34:46 -0300

    atualizado curriculo mobile

--- maiki/src/app/curriculo/curriculo.component.ts
+++ maiki/src/app/curriculo/curriculo.component.ts
@@ -1,3 +1,4 @@
+import { BrowserDetectService } from './../core/services/browser-detect.service';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
@@ -6,10 +7,15 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./curriculo.component.css']
 })
 export class CurriculoComponent implements OnInit {
+  isDesktop = false;
+  isMobile = false;
 
-  constructor() { }
+  constructor(private browserDetect: BrowserDetectService) { }
 
   ngOnInit() {
+    this.isDesktop = this.browserDetect.isDesktop();
+    this.isMobile = this.browserDetect.isMobile();
+    window.onscroll = () => { this.browserDetect.scrollFunction(1010); };
   }
 
 }
